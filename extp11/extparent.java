1d1353ba5de57c30e326b03fb2e3a7e1ad7bc682
Het Kachhadia <Het.kachhdia29@gmail.com>
2025-06-14 19:50:17 +0530

    Add files via upload

---
+++ extp11/extparent.java
@@ -0,0 +1,12 @@
+package extp11;
+import p11.parent;
+public class extparent extends p11.parent
+{
+	public static void main(String arg[])
+	{
+		//System.out.println("default = "+p);
+		System.out.println("public = "+q);
+		//System.out.println("private = "+r);
+		System.out.println("protected = "+s);
+	}
+}
---
+++ extp11/extunknown.java
@@ -0,0 +1,12 @@
+package extp11;
+import p11.parent;
+public class extunknown
+{
+	public static void main(String arg[])
+	{
+		//System.out.println("default = "+x.p);
+		System.out.println("public = "+parent.q);
+		//System.out.println("private = "+x.r);
+		//System.out.println("protected = "+x.s);
+	}
+}
